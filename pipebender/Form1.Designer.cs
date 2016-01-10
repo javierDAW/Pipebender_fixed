d958943a5587771328b231735bdbebd268e12fe6
Jesús Salvador Fuentes Linares <chusrubi2@hotmail.com>
2016-01-10 19:20:55 +0100

    bugs fixed by xavi

---
+++ pipebender/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace pipebender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuImages = new System.Windows.Forms.ImageList(this.components);
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.buttonSaveAs = new System.Windows.Forms.Button();
+            this.buttonPipe = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.mapBox = new System.Windows.Forms.PictureBox();
+            this.regSplitterBtn = new System.Windows.Forms.Button();
+            this.normalSplitterBtn = new System.Windows.Forms.Button();
+            this.mergerBtn = new System.Windows.Forms.Button();
+            this.sinkBtn = new System.Windows.Forms.Button();
+            this.pumpBtn = new System.Windows.Forms.Button();
+            this.buttonClean = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.mapBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuImages
+            // 
+            this.menuImages.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("menuImages.ImageStream")));
+            this.menuImages.TransparentColor = System.Drawing.Color.Transparent;
+            this.menuImages.Images.SetKeyName(0, "Pump.png");
+            this.menuImages.Images.SetKeyName(1, "Sink.png");
+            this.menuImages.Images.SetKeyName(2, "NormalSplitter.png");
+            this.menuImages.Images.SetKeyName(3, "RegulableSplitter.png");
+            this.menuImages.Images.SetKeyName(4, "Merger.png");
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            this.openFileDialog.Filter = " (*.txt)|";
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonOpen.Location = new System.Drawing.Point(787, 12);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 9;
+            this.buttonOpen.Text = "Open ";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonSaveAs
+            // 
+            this.buttonSaveAs.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSaveAs.Location = new System.Drawing.Point(868, 12);
+            this.buttonSaveAs.Name = "buttonSaveAs";
+            this.buttonSaveAs.Size = new System.Drawing.Size(75, 23);
+            this.buttonSaveAs.TabIndex = 10;
+            this.buttonSaveAs.Text = "Save As";
+            this.buttonSaveAs.UseVisualStyleBackColor = true;
+            this.buttonSaveAs.Click += new System.EventHandler(this.buttonSaveAs_Click);
+            // 
+            // buttonPipe
+            // 
+            this.buttonPipe.BackgroundImage = global::pipebender.Properties.Resources.pipe;
+            this.buttonPipe.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.buttonPipe.Location = new System.Drawing.Point(339, 12);
+            this.buttonPipe.Name = "buttonPipe";
+            this.buttonPipe.Size = new System.Drawing.Size(60, 57);
+            this.buttonPipe.TabIndex = 8;
+            this.buttonPipe.UseVisualStyleBackColor = true;
+            this.buttonPipe.Click += new System.EventHandler(this.buttonPipe_Click);
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.BackgroundImage = global::pipebender.Properties.Resources.remove;
+            this.buttonRemove.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.buttonRemove.Location = new System.Drawing.Point(405, 12);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(61, 57);
+            this.buttonRemove.TabIndex = 7;
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.removeBtn);
+            // 
+            // mapBox
+            // 
+            this.mapBox.Location = new System.Drawing.Point(12, 75);
+            this.mapBox.Name = "mapBox";
+            this.mapBox.Size = new System.Drawing.Size(931, 496);
+            this.mapBox.TabIndex = 5;
+            this.mapBox.TabStop = false;
+            this.mapBox.Paint += new System.Windows.Forms.PaintEventHandler(this.mapBox_Paint);
+            this.mapBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.mapBox_MouseClick);
+            // 
+            // regSplitterBtn
+            // 
+            this.regSplitterBtn.BackgroundImage = global::pipebender.Properties.Resources.RegulableSplitter;
+            this.regSplitterBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.regSplitterBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.regSplitterBtn.ForeColor = System.Drawing.SystemColors.Window;
+            this.regSplitterBtn.Location = new System.Drawing.Point(207, 12);
+            this.regSplitterBtn.Name = "regSplitterBtn";
+            this.regSplitterBtn.Size = new System.Drawing.Size(62, 57);
+            this.regSplitterBtn.TabIndex = 4;
+            this.regSplitterBtn.UseVisualStyleBackColor = true;
+            this.regSplitterBtn.Click += new System.EventHandler(this.regSplitterBtn_Click);
+            // 
+            // normalSplitterBtn
+            // 
+            this.normalSplitterBtn.BackgroundImage = global::pipebender.Properties.Resources.NormalSplitter;
+            this.normalSplitterBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.normalSplitterBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.normalSplitterBtn.ForeColor = System.Drawing.SystemColors.Window;
+            this.normalSplitterBtn.Location = new System.Drawing.Point(78, 12);
+            this.normalSplitterBtn.Name = "normalSplitterBtn";
+            this.normalSplitterBtn.Size = new System.Drawing.Size(58, 57);
+            this.normalSplitterBtn.TabIndex = 3;
+            this.normalSplitterBtn.UseVisualStyleBackColor = true;
+            this.normalSplitterBtn.Click += new System.EventHandler(this.normalSplitterBtn_Click);
+            // 
+            // mergerBtn
+            // 
+            this.mergerBtn.BackgroundImage = global::pipebender.Properties.Resources.Merger;
+            this.mergerBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.mergerBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mergerBtn.ForeColor = System.Drawing.SystemColors.Window;
+            this.mergerBtn.Location = new System.Drawing.Point(142, 12);
+            this.mergerBtn.Name = "mergerBtn";
+            this.mergerBtn.Size = new System.Drawing.Size(59, 57);
+            this.mergerBtn.TabIndex = 2;
+            this.mergerBtn.UseVisualStyleBackColor = true;
+            this.mergerBtn.Click += new System.EventHandler(this.mergerBtn_Click);
+            // 
+            // sinkBtn
+            // 
+            this.sinkBtn.BackgroundImage = global::pipebender.Properties.Resources.Sink;
+            this.sinkBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.sinkBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sinkBtn.ForeColor = System.Drawing.SystemColors.Window;
+            this.sinkBtn.Location = new System.Drawing.Point(275, 12);
+            this.sinkBtn.Name = "sinkBtn";
+            this.sinkBtn.Size = new System.Drawing.Size(59, 57);
+            this.sinkBtn.TabIndex = 1;
+            this.sinkBtn.UseVisualStyleBackColor = true;
+            this.sinkBtn.Click += new System.EventHandler(this.sinkBtn_Click);
+            // 
+            // pumpBtn
+            // 
+            this.pumpBtn.BackgroundImage = global::pipebender.Properties.Resources.Pump;
+            this.pumpBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pumpBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pumpBtn.ForeColor = System.Drawing.SystemColors.Window;
+            this.pumpBtn.Location = new System.Drawing.Point(13, 12);
+            this.pumpBtn.Name = "pumpBtn";
+            this.pumpBtn.Size = new System.Drawing.Size(58, 57);
+            this.pumpBtn.TabIndex = 0;
+            this.pumpBtn.UseVisualStyleBackColor = true;
+            this.pumpBtn.Click += new System.EventHandler(this.pumpBtn_Click);
+            // 
+            // buttonClean
+            // 
+            this.buttonClean.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClean.Location = new System.Drawing.Point(787, 46);
+            this.buttonClean.Name = "buttonClean";
+            this.buttonClean.Size = new System.Drawing.Size(75, 23);
+            this.buttonClean.TabIndex = 11;
+            this.buttonClean.Text = "Clean";
+            this.buttonClean.UseVisualStyleBackColor = true;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Enabled = false;
+            this.buttonSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSave.Location = new System.Drawing.Point(868, 46);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 12;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(955, 583);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonClean);
+            this.Controls.Add(this.buttonSaveAs);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.buttonPipe);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.mapBox);
+            this.Controls.Add(this.regSplitterBtn);
+            this.Controls.Add(this.normalSplitterBtn);
+            this.Controls.Add(this.mergerBtn);
+            this.Controls.Add(this.sinkBtn);
+            this.Controls.Add(this.pumpBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pipebender";
+            ((System.ComponentModel.ISupportInitialize)(this.mapBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button pumpBtn;
+        private System.Windows.Forms.Button sinkBtn;
+        private System.Windows.Forms.Button mergerBtn;
+        private System.Windows.Forms.Button normalSplitterBtn;
+        private System.Windows.Forms.Button regSplitterBtn;
+        private System.Windows.Forms.ImageList menuImages;
+        private System.Windows.Forms.PictureBox mapBox;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Button buttonPipe;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.Button buttonSaveAs;
+        private System.Windows.Forms.Button buttonClean;
+        private System.Windows.Forms.Button buttonSave;
+    }
+}
+
---
+++ pipebender/ModelClass.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pipebender
+{
+    class ModelClass
+    {
+        int id, x, y, type,idPipe;
+        String s;
+        public void openFile(ComponentTable ct, StreamReader sr)
+        {
+            String[] tokens;
+            //first process all components
+            s = sr.ReadLine(); s = sr.ReadLine();
+            while (s != "pipes")
+            {
+                tokens = s.Split();
+
+                type = Convert.ToInt32(tokens[0]);
+
+                switch (type)
+                {
+                    case (int)EnumTypes.Pump:
+                        createPump(ct, tokens);
+                        break;
+                    case (int)EnumTypes.Sink:
+                        createSink(ct, tokens);
+                        break;
+                    case (int)EnumTypes.Merger:
+                        createMerger(ct, tokens);
+                        break;
+                    case (int)EnumTypes.Normal:
+                        createNormal(ct, tokens);
+                        break;
+                    case (int)EnumTypes.Regulable:
+                        createRegulable(ct, tokens);
+                        break;
+                }
+                s = sr.ReadLine();
+            }
+
+            s = sr.ReadLine();
+            while (s != "end")
+            {
+                tokens = s.Split();
+                createPipe(ct, tokens,sr);
+                //s = sr.ReadLine();
+
+            }
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public int IdPipe
+        {
+            get
+            {
+                return idPipe;
+            }
+
+            set
+            {
+                idPipe = value;
+            }
+        }
+
+
+
+        public void createPipe(ComponentTable ct, String[] tokens,StreamReader sr)
+        {
+            
+            idPipe = Convert.ToInt32(tokens[0]);
+            int startComponentID = Convert.ToInt32(tokens[1]);
+            int endComponentID = Convert.ToInt32(tokens[2]);
+            int startType = Convert.ToInt32(tokens[3]);
+            int endType = Convert.ToInt32(tokens[4]);
+            int maxFlow = Convert.ToInt32(tokens[5]);
+            int currentFlow = Convert.ToInt32(tokens[6]);
+
+            Pipe pipe = new Pipe(startComponentID,endComponentID,startType, endType, id, maxFlow);
+            pipe.CurrentFlow = currentFlow;
+
+            //Let´s check if there are intermediate points in the pipe
+            String[] PipesToken;
+            s = sr.ReadLine();
+            
+            if (s == "startpoints")
+            {
+                s = sr.ReadLine();
+                List<Point> pipesCoordAux = new List<Point>();
+
+                while (s != "endpoints")
+                {
+                    PipesToken = s.Split();
+                    int x = Convert.ToInt32(PipesToken[0]);
+                    int y = Convert.ToInt32(PipesToken[1]);
+                    Point point = new Point(x, y);
+                    pipesCoordAux.Add(point);
+                    s = sr.ReadLine();
+
+                }
+                pipe.PointOfPipe = pipesCoordAux;
+                s = sr.ReadLine();
+            }
+            
+            ct.PipeList.Add(pipe);
+            
+        }
+
+        public void saveFile(ComponentTable ct, StreamWriter sw)
+        {
+            sw.WriteLine("components");
+            foreach(Component c in ct.CompList)
+            {
+                sw.WriteLine(c.giveMeYourValuesInText());
+            }
+            sw.WriteLine("pipes");
+            foreach (Pipe p in ct.PipeList)
+            {
+                sw.WriteLine(p.giveMeYourValuesInText());
+                if(p.giveMeYourPointOfPipeValuesinText() != "")
+                {
+                    sw.WriteLine("startpoints");
+                    sw.Write(p.giveMeYourPointOfPipeValuesinText());
+                    sw.WriteLine("endpoints");
+                }
+            }
+            sw.WriteLine("end");
+        }
+        private void createPump(ComponentTable ct, String[] tokens)
+        {
+            id = Convert.ToInt32(tokens[1]);
+            x = Convert.ToInt32(tokens[2]);
+            y = Convert.ToInt32(tokens[3]);
+            int f = Convert.ToInt32(tokens[4]);
+            int isOutAvail = Convert.ToInt32(tokens[5]);
+            Point p = new Point(x, y);
+            Pump pump = new Pump(id, p, type);
+            ct.CompList.Add(pump);
+            pump.Flow = f;
+            pump.IsOutAvailable = isOutAvail;
+        }
+
+        private void createMerger(ComponentTable ct, String[] tokens)
+        {
+            id = Convert.ToInt32(tokens[1]);
+            x = Convert.ToInt32(tokens[2]);
+            y = Convert.ToInt32(tokens[3]);
+            int isIn1Avail = Convert.ToInt32(tokens[4]);
+            int isIn2Avail = Convert.ToInt32(tokens[5]);
+            int isOutAvail = Convert.ToInt32(tokens[6]);
+            int in1Flow = Convert.ToInt32(tokens[7]);
+            int in2Flow = Convert.ToInt32(tokens[8]);
+            int outFlow = Convert.ToInt32(tokens[9]);
+            Point p = new Point(x, y);
+            Merger merger = new Merger(id, p, type);
+            ct.CompList.Add(merger);
+            merger.IsIn1Available = isIn1Avail;
+            merger.IsIn2Available = isIn2Avail;
+            merger.IsOutAvailable = isOutAvail;
+            merger.FlowIn1 = in1Flow;
+            merger.FlowIn2 = in2Flow;
+            merger.GetFlowOut = outFlow;
+        }
+
+        private void createNormal(ComponentTable ct, String[] tokens)
+        {
+            id = Convert.ToInt32(tokens[1]);
+            x = Convert.ToInt32(tokens[2]);
+            y = Convert.ToInt32(tokens[3]);
+            int splitterType = Convert.ToInt32(tokens[4]);
+            int isInAvail = Convert.ToInt32(tokens[5]);
+            int isOut1Avail = Convert.ToInt32(tokens[6]);
+            int isOut2Avail = Convert.ToInt32(tokens[7]);
+            int inFlow = Convert.ToInt32(tokens[8]);
+            int out1Flow = Convert.ToInt32(tokens[9]);
+            int out2Flow = Convert.ToInt32(tokens[10]);
+            Point p = new Point(x, y);
+            Normal normal = new Normal(id, p, splitterType, type);
+            ct.CompList.Add(normal);
+            normal.IsInAvailable = isInAvail;
+            normal.IsOut1Available = isOut1Avail;
+            normal.IsOut2Available = isOut2Avail;
+            normal.GetFlowIn = inFlow;
+            normal.GetFlowOut1 = out1Flow;
+            normal.GetFlowOut2 = out2Flow;
+        }
+
+        private void createRegulable(ComponentTable ct, String[] tokens)
+        {
+            id = Convert.ToInt32(tokens[1]);
+            x = Convert.ToInt32(tokens[2]);
+            y = Convert.ToInt32(tokens[3]);
+            int splitterType = Convert.ToInt32(tokens[4]);
+            int isInAvail = Convert.ToInt32(tokens[5]);
+            int isOut1Avail = Convert.ToInt32(tokens[6]);
+            int isOut2Avail = Convert.ToInt32(tokens[7]);
+            int inFlow = Convert.ToInt32(tokens[8]);
+            int out1Flow = Convert.ToInt32(tokens[9]);
+            int out2Flow = Convert.ToInt32(tokens[10]);
+            int percentage = Convert.ToInt32(tokens[11]);
+            Point p = new Point(x, y);
+            Regulable regulable = new Regulable(id, p, splitterType,percentage, type);
+            ct.CompList.Add(regulable);
+            regulable.IsInAvailable = isInAvail;
+            regulable.IsOut1Available = isOut1Avail;
+            regulable.IsOut2Available = isOut2Avail;
+            regulable.GetFlowIn = inFlow;
+            regulable.GetFlowOut1 = out1Flow;
+            regulable.GetFlowOut2 = out2Flow;
+            regulable.GetFlowOut2 = out2Flow;
+            
+        }
+
+
+        private void createSink(ComponentTable ct, String[] tokens)
+        {
+            id = Convert.ToInt32(tokens[1]);
+            x = Convert.ToInt32(tokens[2]);
+            y = Convert.ToInt32(tokens[3]);
+            int f = Convert.ToInt32(tokens[4]);
+            int isInAvail = Convert.ToInt32(tokens[5]);
+            Point p = new Point(x, y);
+            Sink sink = new Sink(id, p,f, type);
+            ct.CompList.Add(sink);
+            sink.IsInAvailable = isInAvail;
+        }
+    }
+}
